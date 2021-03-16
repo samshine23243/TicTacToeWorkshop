15f85862a6e25c039f75d95d967a523975888afa
samshine <sameershinde23243@gmail.com>
2021-03-16 08:51:50 +0530

    board creation code added

---
+++ TicTacToeGame.java
@@ -0,0 +1,14 @@
+
+public class TicTacToeGame {
+	void createBoard()
+	{
+		char[][] board=new char[3][3];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
